702f5e0b30cefbe189bea4999988929eb66f102f
Conrad Tondryk <208223989+conradtondryk@users.noreply.github.com>
2025-11-17 16:28:02 +0000

    migrate database queries to use @vercel/postgres for automatic ssl handling

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL_NON_POOLING,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: true,
   },
 });
 
